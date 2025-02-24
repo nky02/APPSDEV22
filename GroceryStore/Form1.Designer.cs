5c8073a08cd76e17dd218b5a220300c6975be5da
Basalo, Aldrin Meller T. <150681783+Aden9000@users.noreply.github.com>
2025-02-24 21:04:14 +0800

    Add files via upload

---
+++ GroceryStore/Form1.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace GroceryStore
+{
+    partial class GroceryDeALkanse
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            panel1 = new Panel();
+            label7 = new Label();
+            button1 = new Button();
+            priceTextBox = new TextBox();
+            quanTextBox = new TextBox();
+            itemTextBox = new TextBox();
+            quantityLabel = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            dataGridView1 = new DataGridView();
+            item = new DataGridViewTextBoxColumn();
+            quantity = new DataGridViewTextBoxColumn();
+            price = new DataGridViewTextBoxColumn();
+            delete = new DataGridViewButtonColumn();
+            checkoutButton = new Button();
+            totalCostTxtBox = new TextBox();
+            discountTextBox = new TextBox();
+            totalAmountTextBox = new TextBox();
+            panel2 = new Panel();
+            percentDiscountTxtLabel = new TextBox();
+            discountPrcntLabel = new Label();
+            doneButton = new Button();
+            exitButton = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.AppWorkspace;
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(priceTextBox);
+            panel1.Controls.Add(quanTextBox);
+            panel1.Controls.Add(itemTextBox);
+            panel1.Controls.Add(quantityLabel);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 347);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(363, 344);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Gill Sans Ultra Bold Condensed", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(74, 21);
+            label7.Name = "label7";
+            label7.Size = new Size(210, 27);
+            label7.TabIndex = 7;
+            label7.Text = "GROCERY DE ALKANSE";
+            label7.Click += label7_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(144, 280);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 6;
+            button1.Text = "ADD";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += addButton_Click;
+            // 
+            // priceTextBox
+            // 
+            priceTextBox.Location = new Point(197, 197);
+            priceTextBox.Name = "priceTextBox";
+            priceTextBox.Size = new Size(156, 31);
+            priceTextBox.TabIndex = 5;
+            priceTextBox.TextChanged += pricePerUnitTextBox_TextChanged;
+            // 
+            // quanTextBox
+            // 
+            quanTextBox.Location = new Point(197, 139);
+            quanTextBox.Name = "quanTextBox";
+            quanTextBox.Size = new Size(156, 31);
+            quanTextBox.TabIndex = 4;
+            quanTextBox.TextChanged += quanTextBox_TextChanged;
+            // 
+            // itemTextBox
+            // 
+            itemTextBox.Location = new Point(197, 74);
+            itemTextBox.Name = "itemTextBox";
+            itemTextBox.Size = new Size(156, 31);
+            itemTextBox.TabIndex = 3;
+            itemTextBox.TextChanged += itemTextBox_TextChanged;
+            // 
+            // quantityLabel
+            // 
+            quantityLabel.AutoSize = true;
+            quantityLabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            quantityLabel.Location = new Point(25, 139);
+            quantityLabel.Name = "quantityLabel";
+            quantityLabel.Size = new Size(105, 25);
+            quantityLabel.TabIndex = 2;
+            quantityLabel.Text = "QUANTITY";
+            quantityLabel.Click += quantityLabel_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(16, 197);
+            label2.Name = "label2";
+            label2.Size = new Size(134, 25);
+            label2.TabIndex = 1;
+            label2.Text = "PRICE per unit";
+            label2.Click += priceLabel_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(25, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 25);
+            label1.TabIndex = 0;
+            label1.Text = "ITEM NAME";
+            label1.Click += itemLabel_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(27, 36);
+            label3.Name = "label3";
+            label3.Size = new Size(128, 25);
+            label3.TabIndex = 2;
+            label3.Text = "TOTAL COST: ";
+            label3.Click += totalCostLabel_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(24, 119);
+            label4.Name = "label4";
+            label4.Size = new Size(196, 25);
+            label4.TabIndex = 3;
+            label4.Text = "DISCOUNT AMOUNT:";
+            label4.Click += discountLabel_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(363, 388);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 25);
+            label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(27, 162);
+            label6.Name = "label6";
+            label6.Size = new Size(159, 25);
+            label6.TabIndex = 5;
+            label6.Text = "TOTAL AMOUNT:";
+            label6.Click += label6_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { item, quantity, price, delete });
+            dataGridView1.Location = new Point(154, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(666, 275);
+            dataGridView1.TabIndex = 6;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // item
+            // 
+            dataGridViewCellStyle1.NullValue = "₱";
+            item.DefaultCellStyle = dataGridViewCellStyle1;
+            item.HeaderText = "ITEM";
+            item.MinimumWidth = 8;
+            item.Name = "item";
+            item.ReadOnly = true;
+            // 
+            // quantity
+            // 
+            quantity.HeaderText = "QUANTITY";
+            quantity.MinimumWidth = 8;
+            quantity.Name = "quantity";
+            quantity.ReadOnly = true;
+            // 
+            // price
+            // 
+            price.HeaderText = "PRICE";
+            price.MinimumWidth = 8;
+            price.Name = "price";
+            price.ReadOnly = true;
+            // 
+            // delete
+            // 
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = SystemColors.ActiveCaption;
+            dataGridViewCellStyle2.SelectionBackColor = Color.Transparent;
+            dataGridViewCellStyle2.SelectionForeColor = Color.Transparent;
+            delete.DefaultCellStyle = dataGridViewCellStyle2;
+            delete.FlatStyle = FlatStyle.Popup;
+            delete.HeaderText = "DELETE";
+            delete.MinimumWidth = 8;
+            delete.Name = "delete";
+            delete.ReadOnly = true;
+            delete.Text = "delete";
+            delete.UseColumnTextForButtonValue = true;
+            // 
+            // checkoutButton
+            // 
+            checkoutButton.BackColor = SystemColors.ActiveCaption;
+            checkoutButton.Location = new Point(381, 293);
+            checkoutButton.Name = "checkoutButton";
+            checkoutButton.Size = new Size(189, 37);
+            checkoutButton.TabIndex = 7;
+            checkoutButton.Text = "CHECK OUT";
+            checkoutButton.UseVisualStyleBackColor = false;
+            checkoutButton.Click += checkoutButton_Click;
+            // 
+            // totalCostTxtBox
+            // 
+            totalCostTxtBox.Location = new Point(230, 30);
+            totalCostTxtBox.Name = "totalCostTxtBox";
+            totalCostTxtBox.ReadOnly = true;
+            totalCostTxtBox.Size = new Size(176, 31);
+            totalCostTxtBox.TabIndex = 8;
+            totalCostTxtBox.TextChanged += totalCostTxtBox_TextChanged;
+            // 
+            // discountTextBox
+            // 
+            discountTextBox.Location = new Point(230, 113);
+            discountTextBox.Name = "discountTextBox";
+            discountTextBox.ReadOnly = true;
+            discountTextBox.Size = new Size(176, 31);
+            discountTextBox.TabIndex = 9;
+            discountTextBox.TextChanged += discountTextBox_TextChanged;
+            // 
+            // totalAmountTextBox
+            // 
+            totalAmountTextBox.Location = new Point(230, 162);
+            totalAmountTextBox.Name = "totalAmountTextBox";
+            totalAmountTextBox.ReadOnly = true;
+            totalAmountTextBox.Size = new Size(176, 31);
+            totalAmountTextBox.TabIndex = 10;
+            totalAmountTextBox.TextChanged += totalAmountTextBox_TextChanged;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.AppWorkspace;
+            panel2.Controls.Add(percentDiscountTxtLabel);
+            panel2.Controls.Add(discountPrcntLabel);
+            panel2.Controls.Add(doneButton);
+            panel2.Controls.Add(totalAmountTextBox);
+            panel2.Controls.Add(discountTextBox);
+            panel2.Controls.Add(totalCostTxtBox);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Location = new Point(593, 347);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(460, 282);
+            panel2.TabIndex = 11;
+            // 
+            // percentDiscountTxtLabel
+            // 
+            percentDiscountTxtLabel.Location = new Point(230, 71);
+            percentDiscountTxtLabel.Name = "percentDiscountTxtLabel";
+            percentDiscountTxtLabel.ReadOnly = true;
+            percentDiscountTxtLabel.Size = new Size(176, 31);
+            percentDiscountTxtLabel.TabIndex = 13;
+            percentDiscountTxtLabel.TextChanged += textBox1_TextChanged;
+            // 
+            // discountPrcntLabel
+            // 
+            discountPrcntLabel.AutoSize = true;
+            discountPrcntLabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            discountPrcntLabel.Location = new Point(27, 77);
+            discountPrcntLabel.Name = "discountPrcntLabel";
+            discountPrcntLabel.Size = new Size(193, 25);
+            discountPrcntLabel.TabIndex = 12;
+            discountPrcntLabel.Text = "PERCENT DISCOUNT:";
+            discountPrcntLabel.Click += discountPrcntLabel_Click;
+            // 
+            // doneButton
+            // 
+            doneButton.BackColor = SystemColors.ActiveCaption;
+            doneButton.ForeColor = SystemColors.Desktop;
+            doneButton.Location = new Point(179, 222);
+            doneButton.Name = "doneButton";
+            doneButton.Size = new Size(112, 37);
+            doneButton.TabIndex = 11;
+            doneButton.Text = "DONE";
+            doneButton.UseVisualStyleBackColor = false;
+            doneButton.Click += doneButton_Click;
+            // 
+            // exitButton
+            // 
+            exitButton.Location = new Point(425, 663);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(145, 40);
+            exitButton.TabIndex = 12;
+            exitButton.Text = "CLOSE";
+            exitButton.UseVisualStyleBackColor = true;
+            exitButton.Click += exitButton_Click;
+            // 
+            // GroceryDeALkanse
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Beige;
+            ClientSize = new Size(1065, 760);
+            Controls.Add(exitButton);
+            Controls.Add(panel2);
+            Controls.Add(checkoutButton);
+            Controls.Add(dataGridView1);
+            Controls.Add(label5);
+            Controls.Add(panel1);
+            ForeColor = SystemColors.ControlText;
+            Name = "GroceryDeALkanse";
+            Text = "GroceryDeALkanse";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label quantityLabel;
+        private Label label2;
+        private Label label1;
+        private Button button1;
+        private TextBox priceTextBox;
+        private TextBox quanTextBox;
+        private TextBox itemTextBox;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private DataGridView dataGridView1;
+        private Button checkoutButton;
+        private TextBox totalCostTxtBox;
+        private TextBox discountTextBox;
+        private TextBox totalAmountTextBox;
+        private Panel panel2;
+        private Label label7;
+        private Button doneButton;
+        private Label discountPrcntLabel;
+        private TextBox percentDiscountTxtLabel;
+        private Button exitButton;
+        private DataGridViewTextBoxColumn item;
+        private DataGridViewTextBoxColumn quantity;
+        private DataGridViewTextBoxColumn price;
+        private DataGridViewButtonColumn delete;
+    }
+}
---
+++ GroceryStore/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace GroceryStore
+{
+    public partial class GroceryDeALkanse : Form
+    {
+
+        public GroceryDeALkanse()
+        {
+            InitializeComponent();
+        }
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string item = itemTextBox.Text.Trim();
+            string quantityText = quanTextBox.Text.Trim();
+            string priceText = priceTextBox.Text.Trim();
+
+            // Validate item (must contain only letters and spaces)
+            if (string.IsNullOrWhiteSpace(item) || !Regex.IsMatch(item, @"^[a-zA-Z\s]+$"))
+            {
+                MessageBox.Show("Item name must contain only letters.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Validate quantity (must be a positive integer)
+            if (!int.TryParse(quantityText, out int quantity) || quantity <= 0)
+            {
+                MessageBox.Show("Quantity must be a valid positive number.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Validate price (must be a positive decimal number)
+            if (!double.TryParse(priceText, out double pricePerUnit) || pricePerUnit <= 0)
+            {
+                MessageBox.Show("Price must be a valid positive number.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Calculate total price
+            double totalPrice = quantity * pricePerUnit;
+
+            // Add data to DataGridView
+            dataGridView1.Rows.Add(item, quantity, totalPrice);
+
+            // Clear input fields
+            itemTextBox.Clear();
+            quanTextBox.Clear();
+            priceTextBox.Clear();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["Delete"].Index && e.RowIndex >= 0)
+            {
+                // Confirm before deleting
+                DialogResult result = MessageBox.Show("Are you sure you want to remove this Item?", "Confirm Deletion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    dataGridView1.Rows.RemoveAt(e.RowIndex);
+                }
+            }
+        }
+
+        private double calculatePrice()
+        {
+            double sum = 0;
+
+            // Loop through each row in DataGridView
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[2].Value != null) // Ensure the cell is not empty
+                {
+                    if (double.TryParse(row.Cells[2].Value.ToString(), out double totalPrice))
+                    {
+                        sum += totalPrice;
+                    }
+                }
+            }
+
+            return sum;
+        }
+        private int calDiscount()
+        {
+            double totalPrice = calculatePrice();
+
+            if (totalPrice < 100)
+            {
+                return 0;  // No discount if total is less than 100
+            }
+            else if (totalPrice >= 100 && totalPrice < 200)
+            {
+                return 10; // 10% discount for 100 to 199
+            }
+            else if (totalPrice >= 200 && totalPrice <= 500)
+            {
+                return 15; // 15% discount for 200 to 500
+            }
+            else
+            {
+                return 20; // 20% discount for anything above 500
+            }
+        }
+
+
+        private void checkoutButton_Click(object sender, EventArgs e)
+        {
+
+            // Check if there are any items in the DataGridView
+            if (dataGridView1.Rows.Count == 0 || (dataGridView1.Rows.Count == 1 && dataGridView1.Rows[0].IsNewRow))
+            {
+                // Show a MessageBox if no items exist
+                MessageBox.Show("No items to checkout. Please add items to the list.", "Empty Cart", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            double totalPrice = calculatePrice();
+            int discountPercentage = calDiscount();
+            double discountAmount = (totalPrice * discountPercentage) / 100;
+            double finalPrice = totalPrice - discountAmount;
+
+            // Display values with Peso sign and Percent for discount
+            percentDiscountTxtLabel.Text = $"%{calDiscount()}";
+            totalCostTxtBox.Text = $"₱{totalPrice:F2}";
+            discountTextBox.Text = $"₱{discountAmount:F2}";
+            totalAmountTextBox.Text = $"₱{finalPrice:F2}"; // Final price after discount
+        }
+
+        private void quantityLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void itemLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void priceLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void itemTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quanTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pricePerUnitTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void totalCostLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void discountLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void totalCostTxtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void discountTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void totalAmountTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void doneButton_Click(object sender, EventArgs e)
+        {
+            discountTextBox.Clear();
+            totalCostTxtBox.Clear();
+            totalAmountTextBox.Clear();
+            discountTextBox.Clear();
+            dataGridView1.Rows.Clear();
+        }
+
+        private void discountPrcntLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.ExitThread();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
